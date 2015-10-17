6da3baff4448a34b3b6a3ab936b167181b02bac4
Jake Plahn <jake@jakeplahn.com>
2015-10-17 04:16:42 -0700

    added showTip() and hideTip()

--- public_html/accessible_tooltips/tooltips.js
+++ public_html/accessible_tooltips/tooltips.js
@@ -1,4 +1,14 @@
 (function($){
   var $definitions = $('.definition');
   $definitions.find('.tooltip').attr('aria-hidden','true');
+  
+  function showTip() {
+    $(this).find('.tooltip').attr('aria-hidden', 'false');
+  }
+  function hideTip() {
+    $(this).find('.tooltip').attr('aria-hidden', 'true');
+  }
+
+  $definitions.on('mouseover focusin', showTip);
+  $definitions.on('mouseover focusout', hideTip);
 })(jQuery);
