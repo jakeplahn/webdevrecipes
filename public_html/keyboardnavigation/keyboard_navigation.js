cc8d536bf0370e152869b48044eb6493466b2dd2
Jake Plahn <jake@jakeplahn.com>
2015-10-21 04:20:53 -0700

    added javascript

--- public_html/keyboardnavigation/keyboard_navigation.js
+++ public_html/keyboardnavigation/keyboard_navigation.js
@@ -1,4 +1,48 @@
 (function($) {
+  var currentEntry = -1;
+  
+  function getQueryString(name) {
+    var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
+    var r = window.location.search.substr(1).match(reg);
+    var val = null;
+    if (r !==null)
+    {
+      val = unescape(r[2]);
+    }
+    return val;
+  }
+  function getCurrentPageNumber() {
+    return parseInt(getQueryString('page') || 1);
+  }
+  
+  function loadNextPage() {
+    var pageNumber = getCurrentPageNumber() + 1;
+    var url = window.location.href;
+    if (url.indexOf('page=') !== -1) {
+      window.location.href = replacePageNumber(pageNumber);
+    }
+    else {
+      var joinChar = (url.indexOf('?') > -1) ? '&' : '?';
+      window.location.href += joinChar + pageNumber;
+    }
+  }
+
+  function scrollToEntry(entryIndex) {
+    var top = $("#" + $('.entry')[entryIndex].id).offset().top;
+    $('html.body').animate({ scrollTop: top }, 'slow');
+  }
+  function scrollNext() {
+    if($('.entry').size() > currentEntry + 1) {
+      currentEntry++;
+      scrollToEntry(currentEntry);
+    }
+  }
+  function scrollToPrevious() {
+    if(currentEntry > 0) {
+      currentEntry--;
+      scrollToEntry(currentEntry);
+    }
+  }
   $(document).keydown(function(e) {
     if($(document.activeElement)[0] === $(document.body)[0]){
       switch(e.keyCode){
