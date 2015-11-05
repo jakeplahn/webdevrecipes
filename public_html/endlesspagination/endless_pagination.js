18bf73ff1c0143a0205709735adc44135d2cba43
Jake Plahn <jake@jakeplahn.com>
2015-11-05 05:23:30 -0800

    added functions

--- public_html/endlesspagination/endless_pagination.js
+++ public_html/endlesspagination/endless_pagination.js
@@ -1,6 +1,20 @@
-/*globals jQuery,Handlebars*/
+/*globals jQuery,Handlebars,document*/
 (function($) {
   "use strict";
+  var currentPage = 0;
+  var loadingPage = 0;
+  function nextPageWithJSON() {
+    currentPage += 1;
+    var newURL = '/products.json?page=' + currentPage;
+
+    var splitHref = document.URL.split('?');
+    var parameters = splitHref[1];
+    if (parameters) {
+      parameters = parameters.replace(/[?&]page=[^&]*/, '');
+      newURL += '&' + parameters;
+    }
+    return newURL;
+  }
   function loadData(data) {
     $('#content').append(Handlebars.compile("{{#products}} \
       <div class='product'> \
@@ -8,5 +22,15 @@
         <br> \
         <span class='description'>{{description}}</span> \
         </div>{{/products}}")({ products: data }));
+  }
+  function updateContent(response) {
+    loadData(response);
+  }
+  function getNextPage() {
+    if (loadingPage === 0) {
+      loadingPage++;
+      $.getJSON(nextPageWithJSON(), {}, updateContent)
+        .complete(function() { loadingPage--; });
+      }
   }
 }(jQuery));
