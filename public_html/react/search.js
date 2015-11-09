14977004df627ed3086f505407493e93b151f915
Jake Plahn <jake@jakeplahn.com>
2015-11-09 06:07:19 -0800

    added componentDidMount() and getInitialState()

--- public_html/react/search.js
+++ public_html/react/search.js
@@ -1,5 +1,20 @@
 /*globals React*/
 var ProductSearch = React.createClass({
+  componentDidMount: function() {
+    $.ajax({
+      url: '/products.json',
+      dataType: 'json',
+      success: function(data) {
+        this.SetState({data: data});
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
+  },
+  getInitialState: function() {
+    return {data: []};
+  },
   render: function() {
     return (
       <div>
