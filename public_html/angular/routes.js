6e5009d8f1df1efb3d3255bbce79b224f994abaa
Jake Plahn <jake@jakeplahn.com>
2015-11-12 04:34:45 -0800

    added routProvider and link to index

--- public_html/angular/routes.js
+++ public_html/angular/routes.js
@@ -1,1 +1,6 @@
 var ProductsApp = angular.module("products", ["ngResource", "ngRoute"]);
+ProductsApp.config(['$routeProvider', function($routeProvider) {
+  $routeProvider,
+    when("/", { controller: "ProductsCtrl" }).
+    otherwise({ redirectTo: "/" });
+}]);
