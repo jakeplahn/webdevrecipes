dde25b8b5d028e1735646bd926cc9aa964fdc8b6
Jake Plahn <jake@jakeplahn.com>
2015-11-12 05:28:06 -0800

    added products/id route

--- public_html/angular/routes.js
+++ public_html/angular/routes.js
@@ -2,5 +2,9 @@ var ProductsApp = angular.module("products", ["ngResource", "ngRoute"]);
 ProductsApp.config(['$routeProvider', function($routeProvider) {
   $routeProvider.
     when("/", { controller: "ProductsCtrl" }).
+    when("/products/:id", {
+      controller: "ProductCtrl as productCtrl",
+      templateUrl: "/templates/show.html"
+    }).
     otherwise({ redirectTo: "/" });
 }]);
