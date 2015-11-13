4b5f92e64861afb09f8cb86a0c28c08914ca20b1
Jake Plahn <jake@jakeplahn.com>
2015-11-13 07:08:42 -0800

    moved new and edit routes above view detail

--- public_html/angular/routes.js
+++ public_html/angular/routes.js
@@ -3,10 +3,6 @@ var ProductsApp = angular.module("products", ["ngResource", "ngRoute"]);
 ProductsApp.config(['$routeProvider', function($routeProvider) {
   $routeProvider.
     when("/", { controller: "ProductsCtrl" }).
-    when("/products/:id", {
-      controller:  "ProductCtrl as productCtrl",
-      templateUrl: "/angular/templates/show.html"
-    }).
     when("/products/new", {
       controller: "ProductNewCtrl as productFormCtrl",
       templateUrl: "/angular/templates/form.html"
@@ -14,6 +10,10 @@ ProductsApp.config(['$routeProvider', function($routeProvider) {
     when("/products/:id/edit", {
       controller: "ProductEditCtrl as productFormCtrl",
       templateUrl: "/angular/templates/form.html"
+    }).
+    when("/products/:id", {
+      controller:  "ProductCtrl as productCtrl",
+      templateUrl: "/angular/templates/show.html"
     }).
     otherwise({ redirectTo: "/" });
 }]);
