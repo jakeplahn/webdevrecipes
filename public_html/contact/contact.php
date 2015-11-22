abc5b442daec5b96e772cd5f0a69e8c6888f1dd6
Jake Plahn <jake@jakeplahn.com>
2015-11-22 06:51:07 -0800

    fleshing out contact page

---
+++ public_html/contact/contact.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en-US">
+  <head>
+    <meta charset="utf-8">
+    <title>Awesome Web Development - Contact Us</title>
+  </head>
+  <body>
+
+    <h2>Contact Us</h2>
+    <p>
+      Please fill out this quick form to send us an email. We are excited
+      to hear from you!
+    </p>
+
+    <form id="contact-form" action="contact.php" method="post">
+      
+      <label for="name">Name</label>
+      <input class="full-width" type="text" name="name">
+
+      <label for="email">Your Email</label>
+      <input class="full-width" type="text" name="email">
+
+      <label for="subject">Subject</label>
+      <input class="full-width" type="text" name="subject"
+            value="Web Consulting Inquiry">
+    </form>
+  </body>
+</html>
