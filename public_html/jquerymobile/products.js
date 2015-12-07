2d21954ac2d34d93b96e64eafc3903b389a0d11c
Jake Plahn <jake@jakeplahn.com>
2015-12-07 04:31:50 -0800

    added java script

---
+++ public_html/jquerymobile/products.js
@@ -0,0 +1,25 @@
+/*globals jQuery */
+(function($) {
+  var $products_page = $('#products'),
+      $products_list = $('#products-list'),
+      $product_page  = $('product');
+
+  $products_page.bind('pagebeforeshow', function () {
+    $.getJSON('/products.json', function(products) {
+      var $product_list_item;
+      $products_list.html('');
+
+      $.each(products, function(i, product) {
+        $product_list_item = $('<li>').append(
+          $('<a>')
+            .attr('href', '#product')
+            .text(product.name)
+            .data('transition', 'slide')
+        );
+        $products_list.append($product_list_item);
+      });
+    
+      $products_list.listview('refresh');
+    });
+  });
+})(jQuery);
